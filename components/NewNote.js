b45d048a3ced2dafc65be4758018efbec5bd8b7f
Daniel Chaves <daniel.chaves@digivox.com.br>
2020-09-09 12:58:21 -0300

    100%

--- components/NewNote.js
+++ components/NewNote.js
@@ -5,72 +5,73 @@ import {
   View,
   Text,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 
-export default class Note extends React.Component{
-  render(){
-    return(
-        <View key={this.props.keyval} style={styles.note}>
-          <View style={styles.freespace}/>
-          
-            <Text style={styles.noteText}>{this.props.val.note}</Text>
+export default class Note extends React.Component {
+  render() {
+    return (
+      <View key={this.props.keyval} style={styles.note}>
+        <View style={styles.freespace} />
 
-            <TouchableOpacity onPress={this.props.deleteMethod} 
-            style={styles.noteDelete}> 
-                <Text style={styles.noteDeleteText}>Deletar</Text>
-            </TouchableOpacity>
+        <Text style={styles.noteText}>{this.props.val.note}</Text>
 
-            <TouchableOpacity 
-            style={styles.noteQuantity}> 
-                <Text style={styles.noteQuantityText}>Qtd: {this.props.val.qtd}</Text>
-            </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.props.deleteMethod}
+          style={styles.noteDelete}>
+          <Text style={styles.noteDeleteText}>Deletar</Text>
+        </TouchableOpacity>
 
-            <TextInput 
-                onChangeText={(text) => {
-                  this.props.editInput(text)
-                }}
-                placeholder='Qtd'
-                placeholderTextColor='gray'
-                underlineColorAndroid='transparent'
-                style={styles.qtd}>
-            </TextInput>
+        <TouchableOpacity style={styles.noteQuantity}>
+          <Text style={styles.noteQuantityText}>Qtd: {this.props.val.qtd}</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity onPress={this.props.modQuantityMethod}
-            style={styles.mudar}> 
-                <Text style={styles.plus}>Mudar</Text>
-            </TouchableOpacity>
+        <TextInput
+          onChangeText={(text) => {
+            this.props.editInput(text);
+          }}
+          placeholder="_____"
+          placeholderTextColor="gray"
+          underlineColorAndroid="transparent"
+          style={styles.qtd}></TextInput>
 
-            <TouchableOpacity onPress={this.props.addQuantityMethod}
-            style={styles.plusOne}> 
-                <Text style={styles.plus}>+</Text>
-            </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.props.modQuantityMethod}
+          style={styles.mudar}>
+          <Text style={styles.plus}>Mudar</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity  onPress={this.props.removeQuantityMethod}
-            style={styles.minusOne}> 
-                <Text style={styles.minus}>-</Text>
-            </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.props.addQuantityMethod}
+          style={styles.plusOne}>
+          <Text style={styles.plus}>+</Text>
+        </TouchableOpacity>
 
-        </View>
+        <TouchableOpacity
+          onPress={this.props.removeQuantityMethod}
+          style={styles.minusOne}>
+          <Text style={styles.minus}>-</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
-  note:{
-    position: "relative",
+  note: {
+    position: 'relative',
     padding: 20,
     paddingRight: 130,
     marginLeft: 3,
     marginRight: 3,
-    marginBottom: 40,
+    marginBottom: 60,
     height: 110,
     marginTop: -45,
     flex: 1,
     backgroundColor: 'white',
-    borderRadius: 10
+    borderRadius: 10,
   },
-  noteText:{
+  noteText: {
     paddingLeft: 20,
     paddingBottom: 25,
     paddingRight: 20,
@@ -80,7 +81,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'whitesmoke',
     fontSize: 14,
   },
-  noteDelete:{
+  noteDelete: {
     position: 'absolute',
     justifyContent: 'center',
     alignItems: 'center',
@@ -89,11 +90,11 @@ const styles = StyleSheet.create({
     bottom: 10,
     top: -8,
     right: 14,
-    marginTop: 34,   
+    marginTop: 34,
     width: 95,
-    height: 20
+    height: 20,
   },
-  noteQuantity:{
+  noteQuantity: {
     position: 'absolute',
     justifyContent: 'center',
     alignItems: 'center',
@@ -102,61 +103,60 @@ const styles = StyleSheet.create({
     bottom: 10,
     top: 18,
     right: 14,
-    marginTop: 34,   
+    marginTop: 34,
     width: 95,
-    height: 20
+    height: 20,
   },
-  noteDeleteText:{
-    color: 'white'
+  noteDeleteText: {
+    color: 'white',
   },
-  noteQuantityText:{
-    color: 'white'
+  noteQuantityText: {
+    color: 'white',
   },
-  plusOne:{
-    backgroundColor:'#E91E63',
+  plusOne: {
+    backgroundColor: '#E91E63',
     right: 135,
     top: 25,
-    width: 20,
-    height: 20,
+    width: 26,
+    height: 26,
     alignItems: 'center',
-    justifyContent:'center',
-    borderRadius: 16,
-    position: 'absolute'
+    justifyContent: 'center',
+    borderRadius: 18,
+    position: 'absolute',
   },
-  qtd:{
-    position:'absolute', 
-    left: 278, 
-    top: 63, 
+  qtd: {
+    position: 'absolute',
+    left: 280,
+    top: 72,
   },
-  mudar:{
-    backgroundColor:'red',
+  mudar: {
+    backgroundColor: 'red',
     right: 14,
     top: 78,
     width: 60,
     height: 20,
     alignItems: 'center',
-    justifyContent:'center',
-    position: 'absolute'
+    justifyContent: 'center',
+    position: 'absolute',
   },
-  plus:{
-    color:'white'
+  plus: {
+    color: 'white',
   },
-  minusOne:{
-    backgroundColor:'#E91E63',
-    width: 20,
-    height: 20,
+  minusOne: {
+    backgroundColor: '#E91E63',
+    width: 26,
+    height: 26,
     alignItems: 'center',
-    justifyContent:'center',
-    borderRadius: 16,
-    backgroundColor:'#E91E63',
-    position:'absolute',
+    justifyContent: 'center',
+    borderRadius: 18,
+    backgroundColor: '#E91E63',
+    position: 'absolute',
     right: 135,
-    top: 55
+    top: 55,
   },
-  minus:{
-    color:'white',
-    fontSize: 20
+  minus: {
+    color: 'white',
+    fontSize: 20,
   },
-  freespace:{
-  }
+  freespace: {},
 });
--- pages/Caldos/Caldos.js
+++ pages/Caldos/Caldos.js
@@ -9,222 +9,223 @@ import {
   View,
   Text,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 
-export default class Estoque extends React.Component{
-
-  constructor(props){
+export default class Estoque extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       noteArray: [],
       noteText: '',
-    }
+    };
 
     this.input = 0;
 
     const init = async () => {
       try {
-        const jsonValue = await AsyncStorage.getItem('@caldos')
-        if(jsonValue !== null){
+        const jsonValue = await AsyncStorage.getItem('@espetinhos');
+        if (jsonValue !== null) {
           this.setState(JSON.parse(jsonValue));
         }
-        console.log("Sucess!")
-      } catch(e) {
-        console.log(e)
+        console.log('Sucess!');
+      } catch (e) {
+        console.log(e);
       }
-    }
+    };
 
     //Limpar o armazenamento
     //AsyncStorage.clear();
-    
+
     init();
   }
 
-  render(){
-
-    let notes = this.state.noteArray.map((val,key) => {
-      return <Note 
-        key={key} 
-        keyval={key} 
-        val={val}
-        deleteMethod={() => this.deleteNote(key)}
-        addQuantityMethod={() => this.addQuantity(val.id)}
-        removeQuantityMethod={() => this.removeQuantity(val.id)}
-        editInput={(input) => this.modInput(input)}
-        modQuantityMethod={() => this.modQuantity(val.id)}
+  render() {
+    let notes = this.state.noteArray.map((val, key) => {
+      return (
+        <Note
+          key={key}
+          keyval={key}
+          val={val}
+          deleteMethod={() => this.deleteNote(key)}
+          addQuantityMethod={() => this.addQuantity(val.id)}
+          removeQuantityMethod={() => this.removeQuantity(val.id)}
+          editInput={(input) => this.modInput(input)}
+          modQuantityMethod={() => this.modQuantity(val.id)}
         />
+      );
     });
 
     return (
       <>
         <SafeAreaView>
-            <View style={styles.header}>
-                  <Text style={styles.headerText}>Caldos</Text>
-            </View>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Caldos</Text>
+          </View>
 
-            <ScrollView>
-                <View style={styles.scrollContainer}>
-                  {notes}
-                </View>
-            </ScrollView>
+          <ScrollView>
+            <View style={styles.scrollContainer}>{notes}</View>
+          </ScrollView>
 
-            <View style={styles.footer}>
-                <TextInput
-                  onChangeText={(noteText) => this.setState({noteText})}
-                  value={this.state.noteText}
-                  style={styles.textInput}
-                  placeholder='Insira aqui o nome do produto a ser cadastrado'
-                  placeholderTextColor='white'
-                  underlineColorAndroid='transparent'>
-                </TextInput>
-            </View>
+          <View style={styles.footer}>
+            <TextInput
+              onChangeText={(noteText) => this.setState({noteText})}
+              value={this.state.noteText}
+              style={styles.textInput}
+              placeholder="Insira aqui o nome do produto"
+              placeholderTextColor="white"
+              underlineColorAndroid="transparent"></TextInput>
+          </View>
 
-            <TouchableOpacity onPress = { this.addNote.bind(this) } style={styles.addButton}>
-              <Text style={styles.addButtonText}>+</Text>
-            </TouchableOpacity>
-
+          <TouchableOpacity
+            onPress={this.addNote.bind(this)}
+            style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
         </SafeAreaView>
       </>
     );
   }
 
-  async save(){
+  async save() {
     try {
-      const jsonValue = JSON.stringify(this.state)
-      await AsyncStorage.setItem('@caldos', jsonValue)
-      console.log("Salvo!");
+      const jsonValue = JSON.stringify(this.state);
+      await AsyncStorage.setItem('@espetinhos', jsonValue);
+      console.log('Salvo!');
     } catch (e) {
       console.log(e);
     }
   }
 
-  gerarId(){
-    if(this.state.noteArray.length==0){
+  gerarId() {
+    if (this.state.noteArray.length == 0) {
       return 0;
-    }else{
+    } else {
       let id = 0;
       this.state.noteArray.map((produto) => {
-        id = produto.id+1;
+        id = produto.id + 1;
       });
       return id;
     }
   }
 
-  addNote(){
-    if(this.state.noteText){
+  addNote() {
+    if (this.state.noteText) {
       this.state.noteArray.push({
         id: this.gerarId(),
         note: this.state.noteText,
         qtd: 0,
       });
 
-      this.setState({ noteArray: this.state.noteArray });
-      this.setState({ noteText: ''});
+      this.setState({noteArray: this.state.noteArray});
+      this.setState({noteText: ''});
 
       this.save();
     }
   }
 
-  addQuantity(id){
+  addQuantity(id) {
     this.state.noteArray.map((produto) => {
       if (produto.id === id) {
-        produto.qtd = produto.qtd+1;
+        produto.qtd = produto.qtd + 1;
       }
-    })
+    });
 
-    this.setState({ noteArray: this.state.noteArray });
+    this.setState({noteArray: this.state.noteArray});
     this.save();
   }
 
-  modInput(input){
+  modInput(input) {
     this.input = parseInt(input);
     console.log(this.input);
   }
 
-  modQuantity(id){
+  modQuantity(id) {
     this.state.noteArray.map((produto) => {
       if (produto.id === id) {
         produto.qtd = this.input;
       }
-    })
+    });
 
-    this.setState({ noteArray: this.state.noteArray });
+    this.setState({noteArray: this.state.noteArray});
     this.save();
   }
 
-  removeQuantity(id){
+  removeQuantity(id) {
     this.state.noteArray.map((produto) => {
       if (produto.id === id) {
-        if (produto.qtd>0) {
-          produto.qtd = produto.qtd-1;
+        if (produto.qtd > 0) {
+          produto.qtd = produto.qtd - 1;
         }
       }
-    })
+    });
 
-    this.setState({ noteArray: this.state.noteArray });
+    this.setState({noteArray: this.state.noteArray});
     this.save();
   }
 
-  deleteNote(key){
+  deleteNote(key) {
     this.state.noteArray.splice(key, 1);
-    this.setState({ noteArray: this.state.noteArray });
+    this.setState({noteArray: this.state.noteArray});
 
     this.save();
   }
-};
+}
 
 const styles = StyleSheet.create({
-  header:{
-      backgroundColor: '#E91E63',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderBottomWidth: 3,
-      borderBottomColor: '#ddd',
-      padding: 32,
-      flex:1,
+  header: {
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 3,
+    borderBottomColor: '#ddd',
+    padding: 32,
+    flex: 1,
   },
-  headerText:{
-      color: 'white',
-      marginTop: 10,
-      fontSize: 22,
-      padding: 26,    
+  headerText: {
+    color: 'white',
+    marginTop: 10,
+    fontSize: 22,
+    padding: 26,
   },
   scrollContainer: {
-      flex: 1,
-      marginTop: 48,
+    flex: 1,
+    marginTop: 140,
   },
   footer: {
-      position: 'absolute',
-      top: 750,
-      left: 0,
-      right: 0,
-      zIndex: 10,
-      flex: 1
+    position: 'absolute',
+    top: 70,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    flex: 1,
+    width: '75%',
   },
   textInput: {
-      alignSelf: 'stretch',
-      color: '#fff',
-      padding: 24,
-      fontSize: 16,
-      backgroundColor: '#E40000',
-      borderTopWidth: 2,
-      borderTopColor: '#ededed',
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 24,
+    fontSize: 16,
+    backgroundColor: '#E40000',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
+    borderRadius: 14,
+    marginLeft: 10,
   },
-  addButton:{
+  addButton: {
     position: 'absolute',
     zIndex: 11,
-    right: 20,
-    top: 628,
+    right: 10,
+    top: 70,
     backgroundColor: '#F55858',
-    width: 90,
-    height: 90,
+    width: 80,
+    height: 80,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
     elevation: 8,
   },
-  addButton2:{
+  addButton2: {
     position: 'absolute',
     zIndex: 11,
     right: 20,
@@ -237,8 +238,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     elevation: 8,
   },
-  addButtonText:{
+  addButtonText: {
     color: '#fff',
-    fontSize: 24
-  }
+    fontSize: 24,
+  },
 });
